5c9505517ad9c0885f07a4c689a88a7f746d56fa
sebug <sebastian.gfeller@gmail.com>
2017-04-17 15:58:40 +0200

    Something productive to get results.

--- Controllers/LogEntryController.cs
+++ Controllers/LogEntryController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using log_analyzer.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace log_analyzer.Controllers
@@ -12,17 +15,50 @@ namespace log_analyzer.Controllers
 	public class LogEntryController : Controller
 	{
 		private readonly IOptions<LogSource> _optionsAccessor;
+		private readonly IFileProvider _fileProvider;
 
 		public LogEntryController(IOptions<LogSource> optionsAccessor)
 		{
 			this._optionsAccessor = optionsAccessor;
+			this._fileProvider = new PhysicalFileProvider(_optionsAccessor.Value.Directory);
+		}
+
+		public async Task<string> GetContent(Stream readStream)
+		{
+			string ct = await new StreamReader(readStream).ReadToEndAsync();
+
+			readStream.Dispose();
+
+			return ct;
+		}
+
+		public IEnumerable<Task<string>> GetLogFileContents(IDirectoryContents directoryContents)
+		{
+			foreach (var entry in directoryContents)
+			{
+				if (entry.IsDirectory)
+				{
+					var subItems = _fileProvider.GetDirectoryContents(entry.Name);
+					IEnumerable<Task<string>> subTasks = GetLogFileContents(subItems);
+
+					foreach (var st in subTasks)
+					{
+						yield return st;
+					}
+				}
+				else
+				{
+					yield return GetContent(entry.CreateReadStream());
+				}
+			}
 		}
 
 		[HttpGet]
-		public string Get()
+		public async Task<IEnumerable<string>> Get()
 		{
-			Console.Error.WriteLine("Options accessor: " + _optionsAccessor.Value.Directory);
-			return "Oh";
+			var tasks = GetLogFileContents(_fileProvider.GetDirectoryContents(""));
+
+			return await Task.WhenAll(tasks.Take(10));
 		}
 	}
 }
--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using log_analyzer.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -29,6 +30,7 @@ namespace log_analyzer
         {
             // Add framework services.
             services.AddMvc();
+		services.Configure<LogSource>(Configuration.GetSection("LogSource"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
